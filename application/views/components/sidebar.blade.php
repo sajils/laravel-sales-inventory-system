fcf781f0ff100dc99200df9722de3ff62cdd402f
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-05-13 12:01:00 +0800

    Fixed Sass variables so that sidebar colors can be changed easier

--- application/views/components/sidebar.blade.php
+++ application/views/components/sidebar.blade.php
@@ -2,13 +2,13 @@
 
   <li{{ URI::is('/') ? ' class="active"' : '' }}><a href="{{ URL::to('/') }}"><i class="icon icon-white icon-th-large"></i> Dashboard</a></li>
   
-  <li{{ URI::is('feed') ? ' class="active"' : '' }}><a href="{{ URL::to('/') }}"><i class="icon icon-white icon-th-list"></i> Feed</a></li>
+  <li{{ URI::is('feed') ? ' class="active"' : '' }}><a href="{{ URL::to('/feed') }}"><i class="icon icon-white icon-th-list"></i> Feed</a></li>
 
-  <li{{ URI::is('branches') ? ' class="active"' : '' }}><a href="{{ URL::to('/') }}"><i class="icon icon-white icon-map-marker"></i> Branches</a></li>
+  <li{{ URI::is('branches') ? ' class="active"' : '' }}><a href="{{ URL::to('/branches') }}"><i class="icon icon-white icon-map-marker"></i> Branches</a></li>
 
-  <li{{ URI::is('employees') ? ' class="active"' : '' }}><a href="{{ URL::to('/') }}"><i class="icon icon-white icon-group"></i> Employees</a></li>
+  <li{{ URI::is('employees') ? ' class="active"' : '' }}><a href="{{ URL::to('/employees') }}"><i class="icon icon-white icon-group"></i> Employees</a></li>
 
-  <li{{ URI::is('products') ? ' class="active"' : '' }}><a href="{{ URL::to('/') }}"><i class="icon icon-white icon-tags"></i> Products</a></li>
+  <li{{ URI::is('products') ? ' class="active"' : '' }}><a href="{{ URL::to('/products') }}"><i class="icon icon-white icon-tags"></i> Products</a></li>
   
   <li{{ URI::is('users') ? ' class="active"' : '' }}><a href="{{ route('users') }}"><i class="icon icon-white icon-user"></i> Users</a></li>
 
